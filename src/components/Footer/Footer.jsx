5d831d74719b61f0db66c3bc28a08126b72ccbb3
Jimmy Vu Tran <38023521+JaayTran@users.noreply.github.com>
2023-09-26 00:47:39 -0400

    update footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <a href="https://www.linkedin.com/in/jaaytran/">Created by Jimmy Tran</a>
+    </div>
+  );
 }
 
 export default Footer;
